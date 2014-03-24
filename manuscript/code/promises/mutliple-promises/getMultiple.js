434f96d2fb5cdd4667b58ce0571829121fcd227a
Konstantin <kkonstantin@gmx.de>
2014-03-24 17:39:34 +0100

    Überarbeiten auf $q.all()

--- manuscript/code/promises/mutliple-promises/getMultiple.js
+++ manuscript/code/promises/mutliple-promises/getMultiple.js
@@ -6,16 +6,15 @@
 function getAllTodos(arrToDos) {
     var deferred = $q.defer();
     var intTodoCount = arrToDos.length; // Schneller als ständig über das Array die Länge zu fragen.
-    var arrToDoReady = []; //Hier merken wir uns alle Todos, die fertig geladen sind.
+    var arrPromises = []; //Hier merken wir uns alle Todos, die fertig geladen sind.
 
     for(var index in arrToDos){
-        getTodo(arrTodos[index])  //Aufrufen der Funktion, die genau ein ToDo abruft.
-            .then(function(objTodo){   //Diese liefert im Erfolgsfall genau das ToDo
-                arrToDoReady.push(objTodo); //Das ToDo in unsere 'Callback-Warteschlange' einreihen
-                // Sollte die Warteschlange nun genau so lang sein, wie unser übergebenes Array
-                if(arrToDoReady.length == intTodoCount){
-                    deferred.resolve(arrToDos); // Dann liefere ein 'resolve' und damit den Erfolg.
-                }
-            }, deferred.reject);
+        var promise = getTodo(arrTodos[index]);  //Aufrufen der Funktion, die genau ein ToDo abruft.
+        arrPromises.push(promise); // Auf das Promise Array pushen
     }
+    $q.all(arrPromises).then(function(){ // Auf alle Promises warten
+        deferred.resolve(arrToDos);  // Wenn alle Promises erfolgreich fertig geladen, dann Erfolg weitergeben.
+    }, deferred.reject);  // Sollte ein Promise ein 'reject' werfen, dann abbrechen und 'reject' werfen.
+
+    return deferred.promise;
 }
